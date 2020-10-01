9799754748bfefbc7dd6adb1228e6a85851181e5
adechan <ade.enter@yahoo.com>
2020-10-01 14:29:53 +0300

    Open any link in a new tab.

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,14 @@ import github from "../../assets/github-icon.svg";
 import "./Card.css";
 
 function Card({ image, languages, title, githubUrl, demo }) {
+  // Open any link in a new tab
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) {
+      newWindow.opener = null;
+    }
+  };
+
   return (
     <div className="card">
       <img className="card__image" src={image} alt="" />
@@ -30,10 +38,10 @@ function Card({ image, languages, title, githubUrl, demo }) {
       </div>
       <h3 className="card__title">{title}</h3>
       <div className="card__info">
-        <a href={githubUrl}>
+        <a onClick={() => openInNewTab(githubUrl)}>
           <img src={github} alt="" />
         </a>
-        <a href={demo}>Demo</a>
+        <a onClick={() => openInNewTab(demo)}>Demo</a>
       </div>
     </div>
   );
